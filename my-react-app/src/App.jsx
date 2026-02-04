b4e536ff6fee8a3dd3f76f8dbb3fb4cd32d1c1d5
Vincent <vincentong1997@gmail.com>
2026-02-04 23:42:08 +0800

    Initial Commit

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,9 @@
+import HomePage from './Main Page/HomePage.jsx';
+
+function App() {
+  return <HomePage />;
+}
+
+export default App;
+
+// Notes: The Router
--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -1,11 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import "./Main/index.css";
-import App from "./Main/HomePage.jsx";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './Main Page/index.css'; 
 
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
-)
+  </React.StrictMode>
+);
+
+// Notes: The Path root 
